19373d827990ef312468ab367199c9fcf6c3bee4
touhidul-islam-html <ti360109@gmail.com>
2023-08-29 22:43:53 +0600

    private

---
+++ java/main.java
@@ -0,0 +1,106 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import java.util.Scanner; //use this import function for getting input
+
+//biggest number among trhee numbers
+class BiggestNumberFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int biggestNumber = findBiggestNumber(num1, num2, num3);
+
+        System.out.println("The biggest number is: " + biggestNumber);
+
+        scanner.close();
+    }
+
+    public static int findBiggestNumber(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+}
+
+
+//Area of triangle
+class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+        double area = calculateTriangleArea(base, height);
+
+        System.out.println("The area of the triangle is: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateTriangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+} */
---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+function setInnerText() {
+    var inputValue = document.getElementById("myInput").value;
+    var secret = document.getElementById("secret")
+    var panel = document.getElementById("loginpanel");
+    var hiden = document.getElementById("hiden");
+
+
+
+    if (inputValue == "77"){
+        panel.style.display="none"
+        hiden.style.display="block"
+        secret.innerText ="I am going on Monday"
+    }
+        else{
+        window.close();
+    }
+
+}
